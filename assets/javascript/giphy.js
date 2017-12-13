f2435a062a4db16f0055bc1edafbc1cfad39ed9e
nlw113 <nathanworford@gmail.com>
2017-12-13 14:57:30 -0700

    working

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,6 +1,44 @@
+$(document).ready(function(){
+
+
+
+
+let topics = ["Rugrats","Hey Arnold", "Rocket Power"];
+
+$("#add_button_box button").click(addButton);
+
+
+
+ getButtons();
+function getButtons(){
+
+
+	let html_gif="";
+	for(let i=0; i<topics.length; i++) {
+		html_gif+= "<button>" + topics[i] + "</button>";
+
+
+	}
+
+	$("#button_box").html(html_gif);
+	$("#button_box button").click(function() { 
+		getGIFS($(this).text());
+	});
+};
+
+function addButton(){
+	let searchstr=$("#add_button_box input").val();
+
+	$("#add_button_box input").val("");
+	topics.push(searchstr);
+	getButtons();
+	getGIFS(searchstr);
+}
+
+
 function getGIFS(searchfor){
 
-	var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+searchfor+"&api_key=ykq9N5w7JwsevYTd3IjKuJBHJrD3ONRD");
+	var xhr = $.get("http://api.giphy.com/v1/gifs/search?limit=10&rating=g&q="+searchfor+"&api_key=ykq9N5w7JwsevYTd3IjKuJBHJrD3ONRD");
 	xhr.done(displayGIFS) ;
 
 };
@@ -10,10 +48,14 @@ function displayGIFS(data) {
 	var html = "";
 	
 	for(let i = 0; i < data.data.length;i++) {
+		html+="<figure><figcaption>rating: "+ data.data[i].rating + "</figcaption>";
 		html += "<img src='" + data.data[i].images.fixed_height_still.url + "' data-src='" + data.data[i].images.fixed_height.url + "'> ";
+		html+= "</figure>";
+
 	}
 
-	document.body.innerHTML= html;
+	// document.body.innerHTML= html;
+	$("#gif_box").html(html);
 	$("img").click(function(){
 		var oldsrc = $(this).attr("src");
 		var newsrc = $(this).attr("data-src");
@@ -24,4 +66,4 @@ function displayGIFS(data) {
 
 
 
-getGIFS("federer");
+});
